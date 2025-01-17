3dad5439894961223b8042be07d15373a1d28f9a
SupakornThongaerd <supakorn642@gmail.com>
2025-01-18 00:42:39 +0700

    update project

---
+++ BadmintonCourtAPI/Models/Court.cs
@@ -0,0 +1,6 @@
+public class Court
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public bool IsAvailable { get; set; }
+}
---
+++ BadmintonCourtAPI/Models/Member.cs
@@ -0,0 +1,6 @@
+public class Member
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public int Points { get; set; }
+}
