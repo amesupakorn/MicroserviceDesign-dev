106d2b265684d177fa546e8540f323d8d3e720f0
SupakornThongaerd <supakorn642@gmail.com>
2025-01-19 20:03:59 +0700

    update swagger

--- BadmintonCourtAPI/Program.cs
+++ BadmintonCourtAPI/Program.cs
@@ -3,10 +3,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
+// เพิ่ม Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
+
+// เปิดใช้งาน Swagger
+if (app.Environment.IsDevelopment()) // เปิดเฉพาะตอนพัฒนา
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapControllers();
 
